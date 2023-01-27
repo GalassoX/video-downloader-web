aa1590f53654a8d3998e3ca06b9ea81404b1421c
Rafael <68511324+GalassoX@users.noreply.github.com>
2023-01-27 17:35:43 -0500

    fix modal ui

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,4 @@
-const Modal = ({ open, setOpen, title, body, buttons }) => {
+const Modal = ({ open, onClose, title, body, buttons }) => {
     if (!open) return null;
     return (
         <>
@@ -7,21 +7,21 @@ const Modal = ({ open, setOpen, title, body, buttons }) => {
             >
                 <div className="relative w-auto my-6 mx-auto max-w-3xl">
                     {/*content*/}
-                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none items-center">
                         {/*header*/}
                         {!title
                             ? null
                             :
-                            <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                            <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t w-full">
                                 <h3 className="text-3xl font-semibold">
                                     {title}
                                 </h3>
                                 <button
-                                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                                    onClick={() => setOpen(false)}
+                                    className="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                    onClick={onClose}
                                 >
-                                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                                        ×
+                                    <span className="bg-transparent text-black h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                        X
                                     </span>
                                 </button>
                             </div>
