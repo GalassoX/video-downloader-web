8dbb7dfc7fd86c6ed65e010bff9fb67b474b495f
Rafael <68511324+GalassoX@users.noreply.github.com>
2023-02-28 14:57:58 -0500

    fix top level await error when build

--- src/translate/index.js
+++ src/translate/index.js
@@ -1,3 +1,19 @@
-import { language } from './data';
+import spanish from './langs/es.json';
+import english from './langs/en.json';
 
-export default function translate(code) { return language[code] || 'Translate not defined' }
+const data = [
+    {
+        module: spanish,
+        test: /^es/
+    },
+    {
+        module: english,
+        test: /^en/
+    }
+]
+
+export default function translate(code) {
+    const lang = new Intl.Locale(navigator.language).language;
+    const translate = data.find(d => d.test.test(lang))
+    return translate?.module[code] ?? 'Undefined translate'
+}
